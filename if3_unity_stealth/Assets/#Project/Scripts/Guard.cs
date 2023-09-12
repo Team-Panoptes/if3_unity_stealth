533a48bc0f04a3bd64abcfb03b5059b54b926549
Bastien Gorissen <bastien@teampanoptes.be>
2023-09-12 16:49:31 +0200

    Implemented AI and state machine

--- if3_unity_stealth/Assets/#Project/Scripts/Guard.cs
+++ if3_unity_stealth/Assets/#Project/Scripts/Guard.cs
@@ -5,18 +5,30 @@ using UnityEngine.AI;
 
 public class Guard : MonoBehaviour
 {
-    
+
+    public enum GuardState
+    {
+        Patrol,
+        Chase
+    }
+
+    public GuardState state;
+
     NavMeshAgent agent;
     Transform player;
     public Transform waypoints;
 
     Transform currentTarget;
 
+    public float maxViewDistance = 5;
+    public float maxViewAngle = 45;
+
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         player = GameObject.FindWithTag("Player").transform;
+        state = GuardState.Patrol;
         SelectWaypoint();
     }
 
@@ -24,27 +36,75 @@ public class Guard : MonoBehaviour
     void Update()
     {
 
+        switch (state)
+        {
+            case GuardState.Patrol:
+
+                if(IsPlayerVisible()) {
+                    state = GuardState.Chase;
+                }
+
+                break;
+            case GuardState.Chase:
+                FollowPlayer();
+
+                if(!IsPlayerVisible()) {
+                    state = GuardState.Patrol;
+                    SelectWaypoint();
+                }
+
+                break;
+        }
+
     }
 
-    void FollowPlayer() {
+    bool IsPlayerVisible()
+    {
+        if (Vector3.Distance(transform.position, player.position) <= maxViewDistance)
+        {
+            if (Vector3.Angle(transform.forward, player.position - transform.position) <= maxViewAngle)
+            {
+
+                RaycastHit hit;
+                Vector3 origin = transform.position + Vector3.up;
+
+                if (Physics.Raycast(origin, player.position - transform.position, out hit, maxViewDistance))
+                {
+                    Debug.Log($"{hit.collider.tag} - {hit.collider.name} - {hit.distance}");
+                    if (hit.collider.CompareTag("Player"))
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    void FollowPlayer()
+    {
         // Va vers la position du joueur, navmeshagent
         agent.SetDestination(player.position);
     }
 
-    void SelectWaypoint() {
+    void SelectWaypoint()
+    {
         int index = Random.Range(0, waypoints.childCount);
-        
-        while(waypoints.GetChild(index) == currentTarget) {
+
+        while (waypoints.GetChild(index) == currentTarget)
+        {
             index = Random.Range(0, waypoints.childCount);
         }
-        
+
         currentTarget = waypoints.GetChild(index);
         agent.SetDestination(currentTarget.position);
     }
 
-    void OnTriggerEnter(Collider other) {
-        Debug.Log("ON TRIGGER ENTER!!");
-        if (other.transform == currentTarget) {
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == currentTarget && state == GuardState.Patrol)
+        {
             SelectWaypoint();
         }
     }
